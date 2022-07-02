b432e20d78b7322841f490412b82fff72c120908
ghoulboii <socialmedia@ghoulcloud.slmail.me>
2022-07-03 02:47:43 +0530

    Updating dwmblocks

--- config.h
+++ config.h
@@ -1,8 +1,8 @@
 #define CMDLENGTH 60
-#define DELIMITER " | "
+#define DELIMITER "  "
 
 const Block blocks[] = {
-    BLOCK("sb-battery",  60,    26),
+  BLOCK("sb-battery",  60,    26),
 	BLOCK("sb-volume", 	  0,    25),
 	BLOCK("sb-clock",		   1,	 24),
 };
